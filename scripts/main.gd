86e2220228f36cb989db010bcd9b1d3d23c655b3
MrKatzenGaming <max@langenhan2.de>
2024-11-18 16:16:37 +0100

    change background color based on position

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
+@onready var background: ColorRect = $Background
+@onready var player: Node2D = $Player/CharacterBody2D
+@onready var wall_top: ColorRect = $WallTop
+@onready var wall_bottom: ColorRect = $WallBottom
+
+var factor
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	var diff = (wall_bottom.position.y-2*wall_bottom.size.y) - wall_top.position.y
+	factor = 1/diff
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_released("ctrl_pause"):
-		pass
-
-func pasueMenu():
-	pass
+	background.color.b = clamp(1 - factor*player.position.y,0.3,1)
