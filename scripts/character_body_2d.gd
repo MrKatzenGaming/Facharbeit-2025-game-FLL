24a5f0b3b35e402c77b257234da6aae79242aa99
MrKatzenGaming <max@langenhan2.de>
2024-11-26 19:07:23 +0100

    actually now

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -4,9 +4,6 @@ extends CharacterBody2D
 const SPEED:float = 500.0
 var FlashTime:int = 0
 
-@onready var NodeLayer0:Node2D = get_tree().get_root().get_node("Main").get_node("ObjLayer0")
-@onready var NodeLayer1:Node2D = get_tree().get_root().get_node("Main").get_node("ObjLayer1")
-@onready var NodeLayer2:Node2D = get_tree().get_root().get_node("Main").get_node("ObjLayer2")
 @onready var pause_menu:Control = $"PauseMenu"
 
 @onready var color_rect_2: ColorRect = get_tree().get_root().get_node("Main").get_node("ColorRect2")
@@ -14,15 +11,6 @@ var FlashTime:int = 0
 
 var paused:bool = false
 
-func _ready() -> void:
-
-	NodeLayer0.show()
-	NodeLayer0.process_mode = Node.PROCESS_MODE_ALWAYS
-	
-	NodeLayer1.hide()
-	NodeLayer1.process_mode = Node.PROCESS_MODE_DISABLED
-	NodeLayer2.hide()
-	NodeLayer2.process_mode = Node.PROCESS_MODE_DISABLED
 
 func _physics_process(_delta: float) -> void:
 
@@ -34,18 +22,12 @@ func _physics_process(_delta: float) -> void:
 		print("Debug: ", PlayerVariables.Debug)
 	
 	if PlayerVariables.Debug:
-		$Label.text = "Score: " + str(PlayerVariables.Score) + "\nLayer: " + str(PlayerVariables.Layer)
+		$Label.text = "Score: " + str(PlayerVariables.Score)
 	else:
 		$Label.text = "Score: " + str(PlayerVariables.Score)
 		
 	if Input.is_action_just_pressed("ctrl_pause"):
 		pause_menu.pauseMenu()
-	
-	if Input.is_action_just_pressed("ctrl_layer_up"):
-		changeLayer(0)
-	
-	if Input.is_action_just_pressed("ctrl_layer_down"):
-		changeLayer(1)
 	
 	move_and_slide()
 
@@ -76,38 +58,3 @@ func check_flash() -> void:
 		$Flash/FlashArea.show()
 		color_rect_2.show()
 		canvas_modulate.hide()
-
-func changeLayer(dir: int) -> void:
-	if dir == 1 && PlayerVariables.Layer < 2:
-		PlayerVariables.Layer += 1
-	elif dir == 0 && PlayerVariables.Layer > 0:
-		PlayerVariables.Layer -= 1
-		
-	match PlayerVariables.Layer:
-		0:
-			NodeLayer0.show()
-			NodeLayer0.process_mode = Node.PROCESS_MODE_ALWAYS
-			
-			NodeLayer1.hide()
-			NodeLayer1.process_mode = Node.PROCESS_MODE_DISABLED
-			NodeLayer2.hide()
-			NodeLayer2.process_mode = Node.PROCESS_MODE_DISABLED
-			
-		1:
-			NodeLayer1.show()
-			NodeLayer1.process_mode = Node.PROCESS_MODE_ALWAYS
-			
-			NodeLayer0.hide()
-			NodeLayer0.process_mode = Node.PROCESS_MODE_DISABLED
-			NodeLayer2.hide()
-			NodeLayer2.process_mode = Node.PROCESS_MODE_DISABLED
-			
-		2:
-			NodeLayer2.show()
-			NodeLayer2.process_mode = Node.PROCESS_MODE_ALWAYS
-			
-			NodeLayer1.hide()
-			NodeLayer1.process_mode = Node.PROCESS_MODE_DISABLED
-			NodeLayer0.hide()
-			NodeLayer0.process_mode = Node.PROCESS_MODE_DISABLED
-			
--- scripts/singletones/PlayerVariables.gd
+++ scripts/singletones/PlayerVariables.gd
@@ -1,7 +1,6 @@
 extends Node
 
 var Debug:bool = false
-var Layer:int = 0
 
 var Score:float = 0
 
