2038c57f2dd72819e6692d42ebb8be85410dffba
MrKatzenGaming <max@langenhan2.de>
2024-12-07 14:04:31 +0100

    3. treffen fachbetreuer

--- scripts/Object-Colission-Check.gd
+++ scripts/Object-Colission-Check.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@onready var sprite_2d: Sprite2D = $"../Sprite2D"
 
 
 func _on_area_entered(area: Area2D) -> void:
@@ -8,18 +9,13 @@ func _on_area_entered(area: Area2D) -> void:
 		var parent = get_parent()
 		
 		call_deferred("disable_node",parent)
-		
-		#if parent.is_in_group("Layer0"):
-			#PlayerVariables.add_score(10)
-		#elif parent.is_in_group("Layer1"):
-			#PlayerVariables.add_score(20)
-		#elif parent.is_in_group("Layer2"):
-			#PlayerVariables.add_score(30)
 			
 		if parent.is_in_group("Layer0"):
 			PlayerVariables.add_score(parent.position.y/100)
 
 func disable_node(parent:Node) -> void:
-		parent.hide()
-		parent.process_mode = Node.PROCESS_MODE_DISABLED
-		process_mode = Node.PROCESS_MODE_DISABLED
+		#parent.hide()
+		#parent.process_mode = Node.PROCESS_MODE_DISABLED
+		#process_mode = Node.PROCESS_MODE_DISABLED
+		sprite_2d.modulate.a = 0.25
+		
--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -8,6 +8,7 @@ var FlashTime:int = 0
 
 @onready var color_rect_2: ColorRect = get_tree().get_root().get_node("Main").get_node("ColorRect2")
 @onready var canvas_modulate: CanvasModulate = get_tree().get_root().get_node("Main").get_node("CanvasModulate")
+@onready var flash_light: PointLight2D = $Flash/FlashLight
 
 var paused:bool = false
 
@@ -38,23 +39,25 @@ func check_movement() -> void:
 	if direction:
 		velocity = direction * SPEED
 	else:
-		velocity.y = move_toward(velocity.y , 0 , SPEED)
-		velocity.x = move_toward(velocity.x , 0 , SPEED)
+		velocity.y = move_toward(velocity.y , 0 , 20)
+		velocity.x = move_toward(velocity.x , 0 , 20)
 
 func check_flash() -> void:
 	if Input.is_action_just_pressed("ctrl_flash"):
 		$PlayerCam.screenshot()
 		await RenderingServer.frame_post_draw
-		FlashTime = 10
+		FlashTime = 30
 	
 	if FlashTime == 0:
 		$Flash/FlashArea.process_mode = Node.PROCESS_MODE_DISABLED
 		$Flash/FlashArea.hide()
-		color_rect_2.hide()
-		#canvas_modulate.show()
+		#color_rect_2.hide()
+		##canvas_modulate.show()
+		flash_light.energy = 1
 	else:
 		FlashTime -= 1
 		$Flash/FlashArea.process_mode = Node.PROCESS_MODE_ALWAYS
 		$Flash/FlashArea.show()
-		color_rect_2.show()
-		canvas_modulate.hide()
+		#color_rect_2.show()
+		#canvas_modulate.hide()
+		flash_light.energy = 1000
