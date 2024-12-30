a1454b194c7fe2d693b2d4d075f479e0a6baf81c
MrKatzenGaming <max@langenhan2.de>
2024-12-30 18:16:39 +0100

    work on sonar

--- scripts/sonar.gd
+++ scripts/sonar.gd
@@ -1,12 +1,15 @@
 extends Sprite2D
 
-var timer = 1000
+var timer
+var timer2
 @onready var cooldown: Timer = $"../CooldownTimer"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalBus.sonar_entered.connect(_on_solar_entered)
+	self.scale = Vector2(0,0)
+	self.process_mode = Node.PROCESS_MODE_DISABLED
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -19,13 +22,21 @@ func activate() -> void:
 		cooldown.start()
 		PlayerVariables.AbilityCount -= 1
 		
-	
-		
 func _on_solar_entered() -> void:
 	timer = 0
+	timer2 = 25
+	while timer2 >= 1:
+		timer2 -= 1
+		self.self_modulate.a -= 0.05
+		await get_tree().create_timer(0.01).timeout
+	
+	self.scale = Vector2(0,0)
+	self.process_mode = Node.PROCESS_MODE_DISABLED
 	
 func _run() -> void:
-	self.scale = Vector2(1,1)
+	self.process_mode = Node.PROCESS_MODE_INHERIT
+	self.self_modulate.a = 0.5
+	self.scale = Vector2(0.0,0.0)
 	timer = 1000
 	while timer >=1:
 		self.scale += Vector2(0.05,0.05)
